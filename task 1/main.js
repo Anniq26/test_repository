f175e70c3d31889b60d979213a4920239b52e64e
Anniq26 <aniqitoshvili2012@gmail.com>
2024-02-10 21:38:31 +0400

    first task DOM

--- task 1/main.js
+++ task 1/main.js
@@ -2,25 +2,19 @@ const root= document.body
 
 // div
 const wrapper= document.createElement('div')
-wrapper.classList.add('card')
+wrapper.classList.add('wrapper')
 
 // h1
-const h2 = document.createElement('h2')
-h2.innerText = 'Gandalf'
-
-// a
-
-const link = document.createElement('a')
-link.href = '#'
-link.textContent = 'Go to profile'
+const h1 = document.createElement('h1')
+h1.classList.add('text')
+h1.innerText = 'welcome to our website'
 
 // button
 const button = document.createElement('button')
 button.classList.add('btn')
 button.innerText = 'Submit'
 
-
-wrapper.append(h2, button, link)
+wrapper.append(h1, button)
 root.appendChild(wrapper)
 
 
