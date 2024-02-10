c7608c9cada7666857723992966fdaf10bf56c25
Anniq26 <aniqitoshvili2012@gmail.com>
2024-02-10 21:31:03 +0400

    add some files to repository

---
+++ main.js
@@ -0,0 +1,26 @@
+const root= document.body
+
+// div
+const wrapper= document.createElement('div')
+wrapper.classList.add('card')
+
+// h1
+const h2 = document.createElement('h2')
+h2.innerText = 'Gandalf'
+
+// a
+
+const link = document.createElement('a')
+link.href = '#'
+link.textContent = 'Go to profile'
+
+// button
+const button = document.createElement('button')
+button.classList.add('btn')
+button.innerText = 'Submit'
+
+
+wrapper.append(h2, button, link)
+root.appendChild(wrapper)
+
+
